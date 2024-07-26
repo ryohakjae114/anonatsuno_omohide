802137dfc66af0fd130ece0a0e2384bca7adee33
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-07-26 13:48:50 +0900

    リファクタ

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
-window.addEventListener('DOMContentLoaded', function(){
+// ファイルが選択されたら実行
+document.getElementById("imageInput").addEventListener('change', function(e){
 
-  // ファイルが選択されたら実行
-  document.getElementById("imageInput").addEventListener('change', function(e){
+  let file_reader = new FileReader();
+  
+  // ファイル内容をBase64にエンコードし、「data:〜」で始まるURL形式で取得
+  file_reader.readAsDataURL(e.target.files[0]);
 
-    let file_reader = new FileReader();
-    
-    // ファイル内容をBase64にエンコードし、「data:〜」で始まるURL形式で取得
-    file_reader.readAsDataURL(e.target.files[0]);
-
-    file_reader.addEventListener('load', function(e) {
-      // img要素をページに挿入
-      let imgElement = document.getElementById('img');
-      imgElement.src = e.target.result;
-    });
+  file_reader.addEventListener('load', function(e) {
+    // img要素をページに挿入
+    let imgElement = document.getElementById('img');
+    imgElement.src = e.target.result;
   });
 });
